617610f6e9640976a435b85551e1da4124da78bb
iwsknj <kenji.iwase.5@gmail.com>
2025-01-13 23:57:45 +0900

    mod: ビルドスクリプト

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ln -s $(pwd)/qmk_firmware/keyboards/keyball ../__qmk__/keyboards/keyball
-qmk compile -j 4 -kb keyball/keyball44 -km via_kenj
+qmk compile -j 4 -kb keyball/keyball44 -km kenj
 mkdir -p tmp
-cp ../__qmk__/keyball_keyball44_via_kenj.hex tmp/keyball44.hex
+cp ../__qmk__/keyball_keyball44_kenj.hex tmp/keyball44.hex
